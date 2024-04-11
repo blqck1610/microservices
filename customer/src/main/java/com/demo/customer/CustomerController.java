6388704a872d9ecea2424dd412b220e8b58e1e11
blqck <tdnguyen16202002@gmail.com>
2024-04-12 03:31:22 +0700

    connect to db

--- customer/src/main/java/com/demo/customer/CustomerController.java
+++ customer/src/main/java/com/demo/customer/CustomerController.java
@@ -1,7 +1,7 @@
 package com.demo.customer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +13,11 @@ public class CustomerController {
 
     CustomerService customerService;
 
+    @PostMapping(value = "/create")
     public void createCustomer(@RequestBody  CustomerRegistrationRequest customerRegister){
         log.info("new customer created {}", customerRegister);
+        customerService.registerCustomer(customerRegister);
+
     }
 
 
--- customer/src/main/java/com/demo/customer/CustomerService.java
+++ customer/src/main/java/com/demo/customer/CustomerService.java
@@ -1,15 +1,19 @@
 package com.demo.customer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerService {
+    CustomerRepository customerRepository;
 
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest){
         Customer customer = Customer.builder()
                 .name(customerRegistrationRequest.name())
                 .email(customerRegistrationRequest.email())
                 .build();
+        customerRepository.save(customer);
     }
 
 
